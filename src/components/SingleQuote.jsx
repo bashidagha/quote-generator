1e39d3fbe2e2c9a117eb135cbdc41622bff1cea2
Farshid Karimi <farshidk49@gmail.com>
2022-08-16 14:04:19 +0430

    author button done

--- src/components/SingleQuote.jsx
+++ src/components/SingleQuote.jsx
@@ -1,19 +1,28 @@
-import './singlequote.css'
+import "./singlequote.css";
 
 const SingleQuote = (props) => {
   return (
-    <div className='single'>
+    <div className="single">
+      <p className="single--quote">{props.quote.quote}</p>
 
-    <p className='single--quote'>
-        {props.quote.quote}
-    </p>
-
-    <div className='single--extra'>
-       <div className='single--author'>{props.quote.author}</div> 
-       <div className='single--genre'>{props.quote.genre}</div> 
+      <div className="single--extra">
+        <div className="single--author">{props.quote.author}</div>
+        <div className="single--genre">{props.quote.genre}</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+          />
+        </svg>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SingleQuote
+export default SingleQuote;
